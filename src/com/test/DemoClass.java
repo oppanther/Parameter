2240eb4c235ec6c407824afb405891f57d866ef5
Panther <panther@Panthers-Air.lan>
2021-09-12 21:46:43 -0400

    this is mt first commit

---
+++ src/com/test/DemoClass.java
@@ -0,0 +1,67 @@
+package com.test;
+
+public class DemoClass {
+
+	void getFastName(String fastName) {
+
+		System.out.println(fastName);
+
+	}
+
+	void getLastName(String lastName) {
+		System.out.println(lastName);
+	}
+
+	void GetuserInfo(String name, int age, double height, String adress, long number, char gender) {
+		System.out.println("My name is:" + name);
+		System.out.println("My age is:" + age);
+		System.out.println("My height is:" + height);
+		System.out.println("My adress is:" + adress);
+		System.out.println("My number is:" + number);
+		System.out.println("My gender is:" + gender);
+
+	}
+
+	void name(String fullName) {
+		System.out.println(fullName);
+	}
+
+	void age(int A) {
+		System.out.println(A);
+
+	}
+
+	void height(double B) {
+		System.out.println(B);
+
+	}
+
+	void number(long C) {
+		System.out.println(C);
+
+	}
+
+	void gender(char D) {
+		System.out.println(D);
+		
+
+	}
+
+	void sotti(boolean E) {
+		System.out.println(E);
+
+	}
+
+	void choto(short F) {
+		System.out.println(F);
+
+	}
+
+	void flat(float G) {
+		System.out.println(G);
+
+	}
+	void tmp (byte H) {
+		System.out.println(H);
+	}
+}
---
+++ src/com/test/SmokeTest.java
@@ -0,0 +1,21 @@
+package com.test;
+
+public class SmokeTest {
+
+	public static void main(String[] args) {
+		DemoClass demoClass = new DemoClass();
+		demoClass.getFastName("somun");
+		demoClass.getLastName("hossain");
+		demoClass.GetuserInfo("somun", 23, 5.9, "88-25 jamaica", 9393286937L, 'm');
+		demoClass.name("mohammed");
+		demoClass.age(18);
+		demoClass.gender('m');
+		demoClass.choto((short)1000);
+		demoClass.height(5.9);
+		demoClass.number(9293286937l);
+		demoClass.sotti(false);
+		demoClass.flat(123.420f);
+		demoClass.tmp((byte)12);
+
+	}
+}
